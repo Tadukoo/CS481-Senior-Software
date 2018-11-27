2b42353f8d8b474767c53fa081256b097c28be5a
Tadukoo <realtadukoo@gmail.com>
2018-11-27 17:13:37 -0500

    Removed Trigger-Inducing New Lines

--- src/edu/ycp/cs481/servlets/VerifyEmailServlet.java
+++ src/edu/ycp/cs481/servlets/VerifyEmailServlet.java
@@ -1,8 +1,6 @@
-
 package edu.ycp.cs481.servlets;
 
 import java.io.IOException;
-
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,19 +10,15 @@ import javax.servlet.http.HttpSession;
 
 import edu.ycp.cs481.control.UserController;
 
-
-
 @SuppressWarnings("serial")
 public class VerifyEmailServlet extends HttpServlet{
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 		System.out.println("Verify Email Servlet: doGet");
 
-		
 		String email = req.getParameter("email");
 		String token = req.getParameter("token");
 		UserController uc = new UserController();
-		
 		boolean verify = uc.verifyUser(email, token);
 		HttpSession session = req.getSession();
 		
@@ -36,4 +30,3 @@ public class VerifyEmailServlet extends HttpServlet{
         resp.sendRedirect(req.getContextPath() + "/login");
 	}
 }
-
