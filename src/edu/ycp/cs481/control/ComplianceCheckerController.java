202bc6cf54b49c39cc9bb06d56e8b9555bed13c8
Dglispy <dglispy@ycp.edu>
2018-11-29 15:27:21 -0500

    Quick changes
    still needs alot of work

--- src/edu/ycp/cs481/control/ComplianceCheckerController.java
+++ src/edu/ycp/cs481/control/ComplianceCheckerController.java
@@ -12,51 +12,37 @@ import edu.ycp.cs481.control.SOPController;
 import edu.ycp.cs481.model.Position;
 import edu.ycp.cs481.model.SOP;
 
-public class SystemSnifferController {
+public class ComplianceCheckerController {
 	private Database db = new Database();
 	private static User u; 
 	private static Position p;
 	private static PositionController pc = new PositionController(); 
 	private static UserController uc = new UserController();
 	private static SOPController sc = new SOPController(); 
-	
-	public SystemSnifferController() {
-	
+
+	public ComplianceCheckerController() {
+
 	}
-	
-	//rework into the manager or admin pushes a button and it searches through the requisite user positoins 
-	
-	//use session data to get if a user has certain permissions 
-	public static void getPermissions(HttpServletRequest req) {
-		HttpSession session = req.getSession();
-		String id = (String) session.getAttribute("user_id");
-		int user_id = Integer.parseInt(id);
-		
-		u = uc.searchForUsers(user_id, -1, false, "", false, "", false, "", 0, -1).get(0); 
-		p = u.getPosition();
-		
-		//do a permissions check here
-	}
-	
+
 	public void setAndShowToDoList(Position p) {
 		List<SOP> displayList = p.getIncompleteSOPs();
 		for(SOP s: displayList) {
 			System.out.println(s.getID() + " | " + s.getTitle() + " | " + s.getDescription());
 		}
 	}
-	
+
 	public void setAndshowDoneList(Position p) {
 		List<SOP> displayList = p.getCompletedSOPs();
-		
+
 		for(SOP s: displayList) {
 			System.out.println(s.getID() + " | " + s.getTitle() + " | " + s.getDescription());
 		}
 	}
-	
+
 	public boolean checkIfToDoIsEmpty(Position p) {
 		//set the initial condition to false and change it if need be
 		boolean areGaps = false;
-		
+
 		if(p.getIncompleteSOPs().isEmpty()) {
 			System.out.println("There is no incomplete SOPs");
 			areGaps = false; 
@@ -64,14 +50,14 @@ public class SystemSnifferController {
 			areGaps = true;
 			System.out.println("There are incomplete SOPs");
 		}
-		
+
 		return areGaps;
 	}
-	
+
 	//time to do the actual testing and checking of peoples training histories  
 	public void SniffDeeply() {
 		List<User> systemUsers = uc.searchForUsers(-1, -1, false, "", false, "", false, "", 0, -1);
-		
+
 		if(systemUsers.isEmpty()) {
 			System.out.println("There was an error searching for users.");
 		}
@@ -84,15 +70,15 @@ public class SystemSnifferController {
 				else {
 					Messenger.send(u.getEmail(), "Incomplete Training", u.getFirstName() + ", you have incomplete SOPs in your training"
 							+ "	history, please complete these as soon as possible. A message will also be sent to your manager. Have a great day.");
-					
-					
+
+
 					//Display the List
 					setAndShowToDoList(p); 
 					//send the users manager an email 
 					//TODO: get the users manager 
-					
+
 				}
-				
+
 			}
 		}
 	}
--- src/edu/ycp/cs481/servlets/ComplianceCheckerServlet.java
+++ src/edu/ycp/cs481/servlets/ComplianceCheckerServlet.java
@@ -8,24 +8,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import edu.ycp.cs481.control.SystemSnifferController;
+import edu.ycp.cs481.control.ComplianceCheckerController;
 import edu.ycp.cs481.control.UserController;
 import edu.ycp.cs481.model.EnumPermission;
 
 @SuppressWarnings("serial")
-public class SystemSnifferServlet extends HttpServlet{
+public class ComplianceCheckerServlet extends HttpServlet{
 	
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 		HttpSession session = req.getSession();
 		if(session.getAttribute("user_id") == null) {
 			resp.sendRedirect(req.getContextPath() + "/login");
-		}else{
+		}
+		else{
 			UserController uc = new UserController();
 			int userID = (int) session.getAttribute("user_id");
 			if(uc.userHasPermission(userID, EnumPermission.ALL)){
 				// Only admins with full permissions can go here ^
-				req.getRequestDispatcher("/search_system.jsp").forward(req, resp);
+				req.getRequestDispatcher("/compliance_checker.jsp").forward(req, resp);
 			}else{
 				session.setAttribute("error", "You don't have permission to Swiffer the Seinor!");
 				resp.sendRedirect(req.getContextPath() + "/user_home");
